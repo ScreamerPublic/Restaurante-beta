b5de05a697caf4dbbf1415f298cf263190c9ffa8
ar73582967@gmail.com <ar73582967@gmail.com>
2022-04-24 23:01:18 -0600

    Se elimino una carpeta sin uso

--- restaurante/application/includes/meta.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
-<title>Restaurantly Bootstrap Template - Index</title>
-<meta content="" name="description">
-<meta content="" name="keywords">
